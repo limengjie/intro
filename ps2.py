d5725d2fc5cb5a84a21450ca97306f2f058d932a
Mengjie Li <rned725@gmail.com>
2014-03-17 22:17:50 -0700

    update ps2

--- ps2.py
+++ ps2.py
@@ -36,5 +36,5 @@ ans = d_equation(num_ngts)
 print (ans)
 keys = m.keys()
 values = m.values()
-#for i in range(len(keys)):
-  #print("%s*6+%s*9+%s*20=%s"%values[i][0],values[i][1],values[i][2],keys[i])
+for i in range(len(keys)):
+  print("%d*6+%d*9+%d*20=%d"%(values[i][0],values[i][1],values[i][2],keys[i]))
